1b3843babe12038d6f21db4b78d1adac0f49129d
Patrick Werbke <Patrick.Werbke@googlemail.com>
2014-09-26 11:54:34 +0200

    Session info

--- hello.php
+++ hello.php
@@ -1,3 +1,17 @@
 <?php
 echo 'hello'.'<br />';
 echo 'tschüß';
+php info();
+session_start();
+// Verwenden Sie bei PHP 4.0.6 oder niedriger $HTTP_SESSION_VARS
+if (!isset($_SESSION['zaehler'])) {
+  $_SESSION['zaehler'] = 0;
+} else {
+  $_SESSION['zaehler']++;
+}
+?>
+
+<p>
+Hallo Besucher, Sie haben diese Seite <?php echo $_SESSION['zaehler']; ?> mal
+aufgerufen.
+</p>
